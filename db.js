21c39bff9b5c72379cd20f61c01d7c5093a8d74e
Nero-z17 <ryanowafeu5@gmail.com>
2026-06-08 14:05:10 +0100

    maj de db.js

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 // db.js
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({ 
-  url: "file:./dev.db" 
+// 1. Configuration du pool de connexion PostgreSQL avec l'URL de Neon
+const pool = new pg.Pool({ 
+  connectionString: process.env.DATABASE_URL 
 });
 
+// 2. Création de l'adaptateur pour PostgreSQL
+const adapter = new PrismaPg(pool);
+
+// 3. Injection de l'adaptateur dans le client Prisma
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
